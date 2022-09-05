cadf0a9a5116276174f5c639a81b12af34adbb3e
Pascal Malouin <pascal.malouin@gmail.com>
2022-09-05 13:41:55 -0400

    ...

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#
+# setup.py
+#   created        : 2022-09-05 17:36:12 UTC
+#   updated        : 2022-09-05 17:36:28 UTC
+#   description    : Python libraries and wrappers for alterEGOlinux
+# ___________________________ { alterEGO Linux: "Open the vault of knowledge" }
+
+
+from distutils.core import setup
+
+setup(name='aelpylib',
+      version='0.0.1',
+      description='Python libraries and wrappers for alterEGOlinux',
+      author='Pascal Malouin (fantomH)',
+      author_email='pascal.malouin@protonmail.com',
+      url='#',
+      packages=['aelpylib.useless'],
+     )
+
+# vim: syntax=python
+# { FIN } __________________________________________________________ ¯\_(ツ)_/¯
