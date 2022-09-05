972490b850fd9352998e002be1cc1a2a0d542505
Pascal Malouin <pascal.malouin@gmail.com>
2022-09-05 15:21:11 -0400

    ...

---
+++ aelpylib/useless/helloworld.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#
+# helloword.py
+#   created        : 2022-09-05 17:44:00 UTC
+#   updated        : 2022-09-05 17:44:07 UTC
+#   description    : "Hello, world!"
+# ___________________________ { alterEGO Linux: "Open the vault of knowledge" }
+
+hello_world = "Hello, world!"
+
+if __name__ == "__main__":
+    print(hello_world)
+
+# vim: syntax=python
+# { FIN } __________________________________________________________ ¯\_(ツ)_/¯
