b36e49e73f05e94911d11787d6276833dcaa3e8c
alterEGOlinux <92615665+alterEGOlinux@users.noreply.github.com>
2022-09-07 19:32:19 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 #
 # setup.py
 #   created        : 2022-09-05 17:36:12 UTC
-#   updated        : 2022-09-05 17:36:28 UTC
+#   updated        : 2022-09-09 17:36:28 UTC
 #   description    : Python libraries and wrappers for alterEGOlinux
 # ___________________________ { alterEGO Linux: "Open the vault of knowledge" }
 
 
 from distutils.core import setup
 
-setup(name='aelpylib',
+setup(name='ael',
       version='0.0.1',
       description='Python libraries and wrappers for alterEGOlinux',
       author='Pascal Malouin (fantomH)',
