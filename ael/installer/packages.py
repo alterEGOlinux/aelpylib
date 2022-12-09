096e518ef9ab29b940d01ca1bd605365aea281be
Pascal Malouin <pascal.malouin@gmail.com>
2022-12-08 23:29:12 -0500

    Updated using lazygit...

--- ael/installer/packages.py
+++ ael/installer/packages.py
@@ -4,7 +4,7 @@
 ##
 ## /python-ael/ael/installer/packages.py
 ##   created        : 2022-11-14 11:34:18 UTC
-##   updated        : 2022-11-14 11:34:18 UTC
+##   updated        : 2022-12-09 04:19:26 UTC
 ##   description    : All packages
 ## ____________________________________________________________________________
 
@@ -13,6 +13,9 @@ Package = namedtuple('Package', ['name', 'repository', 'interface', 'description
 packages = [
     Package('alacritty', 'official', 'CLI',  'A fast, cross-platform, OpenGL terminal emulator', 'terminal', '' ),
     Package('fzf', 'official', 'CLI', 'A command-line fuzzy finder', 'utility', '' ),
+    Package('nikto', 'official', 'CLI', 'A web server scanner which performs comprehensive tests against web servers for multiple items', 'recon', '' ),
+    Package('nmap', 'official', 'CLI', 'Utility for network discovery and security auditing', 'recon', '' ),
     Package('poppler', 'official', 'CLI', 'PDF rendering library based on xpdf 3.0', 'utility', 'Provides `pdftotext`' ),
+    Package('smbclient', 'official', 'CLI', 'Tools to access a server\'s filespace and printers via SMD', 'file explorer', '' ),
     Package('tmux', 'official', 'CLI', 'A terminal multiplexer', 'terminal', ''),
            ]
